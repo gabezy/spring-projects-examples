579a0094f13e80fc9822481770b63155a3d72aa8
gabezy <gabrielmoreira2705@gmail.com>
2024-11-13 11:16:45 -0300

    added cashcard-api-rest

---
+++ cashcard-rest-api/src/main/java/br/com/gabezy/cashcard/CashcardRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabezy.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashcardRestApiApplication.class, args);
+    }
+
+}
---
+++ cashcard-rest-api/src/test/java/br/com/gabezy/cashcard/CashcardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabezy.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
